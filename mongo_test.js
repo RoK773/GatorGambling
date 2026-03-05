d00159623373dae263879634828edb238eb0e4ff
nolanp75 <nolanpruett@yahoo.com>
2026-03-05 15:53:23 -0500

    test mongo connection

--- mongo_test.js
+++ mongo_test.js
@@ -1,13 +1,26 @@
-const { MongoClient } = require("mongodb");
 
-const uri = "mongodb+srv://admin:admin1Password@cluster0.9uypigw.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:admin1Password@cluster0.9uypigw.mongodb.net/?appName=Cluster0";
 
-const client = new MongoClient(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 async function run() {
-  await client.connect();
-  console.log("Connected successfully!");
-  await client.close();
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
-
-run().catch(console.error);
+run().catch(console.dir);
