aaf630a816cf78c734fc47502583c7fa783d4f82
nolanp75 <139793580+nolanp75@users.noreply.github.com>
2026-04-20 18:27:43 -0400

    removed exposed api key

--- pages/api/bracket.js
+++ pages/api/bracket.js
@@ -4,7 +4,7 @@ import { BETTING_PHASES } from './_lib/bettingLifecycle';
 
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
-const uri = process.env.MONGODB_URI || 'mongodb+srv://admin:admin1Password@cluster0.9uypigw.mongodb.net/?appName=Cluster0';
+const uri = process.env.MONGODB_URI;
 const dbName = process.env.MONGODB_SOCCER_DB || 'Soccer_Data';
 const collectionName = process.env.MONGODB_BRACKET_COLLECTION || 'Bracket';
 const gameBetsCollectionName = process.env.MONGODB_GAME_BETS_COLLECTION || 'Game_bets';
